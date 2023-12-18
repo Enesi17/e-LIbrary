4505883310941c2206f5464df86b3d9f94826b81
Enes Smajli <101655765+Enesi17@users.noreply.github.com>
2023-12-18 12:54:24 +0300

    Home.js update
    Home.js update

--- web-app/src/components/Home.js
+++ web-app/src/components/Home.js
@@ -1,34 +1,39 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
+import { useAuth } from "../context/AuthContext";
 
 const Home = () => {
-    
-    async function handleRegister(e) {
-        e.preventDefault();
-        window.location.pathname = "/register"
-    }
+    const { currentUser } = useAuth(false);
 
-    async function handleLogin(e){
+    const handleRegister = (e) => {
         e.preventDefault();
-        window.location.pathname = "/login"
-    }
+        window.location.pathname = "/register";
+    };
 
-    return ( 
-        <div className="home">
-            <h2>Welcom to Our Library Menagment System</h2>
-        <br></br>
-        <br></br>
-        <br></br>
-            <h5>If you are new click this button</h5>
-                <br />
-             <Button onClick={handleRegister}>Register</Button>
-             <br />
-             <br />
-            <h5>If you already have an account click</h5>
-            <br />
-            <Button onClick={handleLogin}>Login</Button>
-        </div>
-     );
-}
- 
+    const handleLogin = (e) => {
+        e.preventDefault();
+        window.location.pathname = "/login";
+    };
+  return (
+      <Container className="home">
+        {currentUser && <Row>
+            <Col md={{ span: 6, offset: 3 }}>
+              <h2>Welcome to Our Library Management System</h2>
+              <br />
+              <h5>If you are new, click this button</h5>
+              <Button onClick={handleRegister}>
+                Register
+              </Button>
+              <br />
+              <br />
+              <h5>If you already have an account, click</h5>
+              <Button onClick={handleLogin}>
+                Login
+              </Button>
+            </Col>
+          </Row>}
+    </Container>
+  );
+};
+
 export default Home;
